10a9a60ff488a8a65299e3b46a6b0313d2680544
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-07-11 14:43:43 +0000

    Create Vehicle.java
    This is a Vehicle class which vehicle information

---
+++ src/Vehicle.java
@@ -0,0 +1,50 @@
+public class Vehicle {
+
+
+	private int vehicleId;
+
+
+	private boolean isRented;
+
+
+	
+
+
+	public Vehicle(int vehicleId, boolean isRented) {
+
+
+		this.vehicleId = vehicleId;
+
+
+		this.isRented = isRented;
+
+
+	}
+
+
+
+	public boolean isRented() {
+
+
+		return isRented;
+
+
+	}
+
+
+
+	@Override
+
+
+	public String toString() {
+
+
+		return "Vehicle [vehicleId=" + vehicleId + ", isRented=" + isRented + "]";
+
+
+	}
+
+
+}
+
+
