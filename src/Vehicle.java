7c545129cbecd0b977bf166094d9e556a9dd4505
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-07-11 14:45:26 +0000

    Fix: Indentation fixed
    File has been properly indented.

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,50 +1,18 @@
 public class Vehicle {
-
-
 	private int vehicleId;
-
-
 	private boolean isRented;
 
-
-	
-
-
 	public Vehicle(int vehicleId, boolean isRented) {
-
-
 		this.vehicleId = vehicleId;
-
-
 		this.isRented = isRented;
-
-
 	}
-
-
-
+	
 	public boolean isRented() {
-
-
 		return isRented;
-
-
 	}
-
-
-
+	
 	@Override
-
-
 	public String toString() {
-
-
 		return "Vehicle [vehicleId=" + vehicleId + ", isRented=" + isRented + "]";
-
-
 	}
-
-
 }
-
-
